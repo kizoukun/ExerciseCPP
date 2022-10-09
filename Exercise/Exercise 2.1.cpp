b2f3e9b39c670c6ef4f75e36a04612412a556418
Yudistira <kizoukungaming@gmail.com>
2022-10-09 09:03:43 +0700

    Test push

--- Exercise/Exercise 2.1.cpp
+++ Exercise/Exercise 2.1.cpp
@@ -5,6 +5,7 @@ int main()
   cout << "Yudhistira Fauzy Achmadarel - ID 001202200081" << endl;
   cout << "Calculate area of a circle with radius 5:" << endl;
   cout << "5 * 5 * 3.14 = " << (5 * 5 * 3.14) << endl;
+  // For the group project
   // Cin Cout
   // Header / data type
   // If Else selection
